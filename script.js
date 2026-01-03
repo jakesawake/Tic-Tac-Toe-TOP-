9545abce03e6a68e7067e8cb7e50dc3622ace940
Jake Canlas <jakechristiancanlas1@gmail.com>
2026-01-03 17:23:49 -0500

    Rough Draft for Tic-Tac-Toe
    - changed the core logic to only deal with number types
    - no more emojis

--- script.js
+++ script.js
@@ -3,115 +3,107 @@
 const gameBoardContainer = document.querySelector(".gameboard-container");
 const gameText = document.querySelector(".text-area");
 
-// - creating gameboard object that has slots 1-9 for the tic tac toe game
-// - creating empty array for clicked elements on our gameboard
+// - creating gameboard object that has a board with empty strings (9 places)
+// - creating winningLines property that has all possible wins
 const gameboard = {
-  board: [1, 2, 3, 4, 5, 6, 7, 8, 9],
-  winningBoard: [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9],
+  board: ["", "", "", "", "", "", "", "", ""],
+  winningLines: [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [0, 4, 8],
     [1, 4, 7],
-    [1, 5, 9],
+    [2, 4, 6],
     [2, 5, 8],
-    [3, 5, 7],
-    [3, 6, 9],
   ],
 };
 
 // creating player factory function to create players
-const player = function (name, symbol, boardChoice) {
+const player = function (name, symbol, playerBoard) {
   return {
     name,
     symbol,
-    boardChoice: [],
+    playerBoard,
   };
 };
 
 // creating players for the game
-const player1 = player("Jake", "🔴");
-const player2 = player("Marcui", "🔵");
+const player1 = player("Jake", "X", []);
+const player2 = player("Marcui", "O", []);
 
-// creating variable for changing game state
-let currplayer = true;
+// making player1 the current player (X)
+let currplayer = player1;
+
+// function for switching player
+// - if the currplayer is player 1 -> then make player2 the current player, else make player1 the current player
+const switchPlayer = () => {
+  currplayer = currplayer === player1 ? player2 : player1;
+};
 
 // creating function to display board
 const displayBoard = function () {
-  gameboard.board.forEach((item) => {
+  gameboard.board.forEach((item, index) => {
     const cell = document.createElement("div");
     cell.classList.add("cell");
-    if (typeof item === "number") {
+    if (item === "") {
       gameBoardContainer.appendChild(cell);
-      cell.textContent = item;
+      cell.dataset.index = index;
     }
   });
 };
 
-// helper function to compare array contents
-// comparing the length of the two array arguements
-// standard for loop to iterate over array elements
-const arrChecker = function (arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) return false;
-  }
-  {
-    // return true;
-    return console.log("Winner");
-  }
-};
-
-// - using the Object.values() method to return a new array containing
-// the values of all of an object's own, enumerable string-keyed properties
-const checkWinner = function (playerObj, gameboardObj) {
-  let playerChoiceArr;
-  let winningBoardArr;
-  let winningBoardCombination;
-  for (const key in playerObj) {
-    if (key === "boardChoice") {
-      playerChoiceArr = playerObj[key];
-      console.log(playerChoiceArr);
+// - function to compare the players playerboard with any winninglines
+// - using some method to check if atleast one arr is a match
+// - for loop to iterate over the innerArr
+// - if the innerArr equals the currPlayersPositions array
+// - log the winner to the console
+const winCheck = function (currPlayersPositions) {
+  winningLinesArr.some((innerArr) => {
+    if (innerArr.length !== currPlayersPositions.length) {
+      return false;
     }
-  }
-  for (const key in gameboardObj) {
-    if (key === "winningBoard") {
-      winningBoardArr = gameboardObj[key];
-      console.log(winningBoardArr);
+    for (let i = 0; i < innerArr.length; i++) {
+      if (innerArr[i] !== currPlayersPositions[i]) {
+        return false;
+      }
     }
-  }
-  winningBoardCombination = winningBoardArr.some((item) => {
-    console.log(item);
-    arrChecker(playerChoiceArr, item);
+    return console.log(`${currplayer.name} is the winner!`);
   });
 };
 
-// - creating function to capture the players box choice
-// - adding event listener to the parent container gameBoardContainer (instead of attaching a listener for every div individually)
-// - e.target to refer to the exact element clicked
-// - .closest() method to travers up the DOM tree from e.target until it finds an ancestor that matches the provided css selector ('.cell')
-// - pushing the clicked element to the empty array in the gameboard object, also converting to number from string
-// TODO: add guard clause for not being able to select already selected cells
-const playerChoice = function () {
-  let clickedEl;
-  gameBoardContainer.addEventListener("click", (e) => {
-    clickedEl = e.target.closest(".cell");
-    if (currplayer) {
-      player1.boardChoice.push(Number(clickedEl.innerHTML));
-      clickedEl.textContent = player1.symbol;
-      currplayer = false;
-      gameText.textContent = "Now it's 🔵 turn!";
-      checkWinner(player1, gameboard);
-    } else if (!currplayer) {
-      player2.boardChoice.push(Number(clickedEl.innerHTML));
-      clickedEl.textContent = player2.symbol;
-      currplayer = true;
-      gameText.textContent = "Now it's 🔴 turn!";
-      checkWinner(player2, gameboard);
-    }
-  });
-};
+// - creating global variables so they can be accessed in the other functions
+// - assigning gameboard.winningLines to variable winningLinesArr so it removes one layer of nesting (the object)
+let clickedEl;
+let clickedElData;
+let currBoard = gameboard.board;
+let winningLinesArr = gameboard.winningLines;
+
+// - event listener on the entire container
+// - storing the click event in clickedEl
+// - explicitly converting the clickedEl.dataset.index to a number (implicitly returns string)
+// - pushing the dataset index to the player1 or player2 playerboard
+// - then passing the arrays to the winCheck() fn to check for win
+// - calling the switchPlayer() fn to switch player
+gameBoardContainer.addEventListener("click", (e) => {
+  clickedEl = e.target.closest(".cell");
+  clickedEl.style.color = "white";
+  clickedEl.style.fontFamily = "system-ui, -apple-system";
+  if (currplayer === player1) {
+    clickedEl.textContent = player1.symbol;
+    clickedElData = Number(clickedEl.dataset.index);
+    player1.playerBoard.push(clickedElData);
+    winCheck(player1.playerBoard, winningLinesArr);
+    switchPlayer();
+    gameText.textContent = "Now it's O turn!";
+  } else if (currplayer === player2) {
+    clickedEl.textContent = player2.symbol;
+    clickedElData = Number(clickedEl.dataset.index);
+    player2.playerBoard.push(clickedElData);
+    winCheck(player2.playerBoard, winningLinesArr);
+    switchPlayer();
+    gameText.textContent = "Now it's X turn!";
+  }
+});
 
 displayBoard();
-playerChoice();
